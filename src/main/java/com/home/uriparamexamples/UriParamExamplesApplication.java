42dee42e2e05c7aa658d35496865cc743576f82d
Avvarus <avvaru79.java@mail.com>
2019-08-21 19:57:55 -0400

    First Commit

---
+++ src/main/java/com/home/uriparamexamples/UriParamExamplesApplication.java
@@ -0,0 +1,16 @@
+package com.home.uriparamexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class UriParamExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UriParamExamplesApplication.class, args);
+    }
+
+
+}
