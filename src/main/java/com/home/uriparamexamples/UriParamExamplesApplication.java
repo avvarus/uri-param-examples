e352378713c3bd66077a998a21a15abc4cf2f9a4
Avvarus <avvaru79.java@mail.com>
2019-08-21 20:19:29 -0400

    First Commit -102

--- src/main/java/com/home/uriparamexamples/UriParamExamplesApplication.java
+++ src/main/java/com/home/uriparamexamples/UriParamExamplesApplication.java
@@ -2,10 +2,7 @@ package com.home.uriparamexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +11,9 @@ public class UriParamExamplesApplication {
     public static void main(String[] args) {
         SpringApplication.run(UriParamExamplesApplication.class, args);
     }
-//http://localhost:8080/get?a=10&b=200
+
+
+    //@PathVariable
 
     @GetMapping(value = "/get/{a}/{b}")
     public String toStrin(@PathVariable int a, @PathVariable int b) {
@@ -23,9 +22,19 @@ public class UriParamExamplesApplication {
     }
 
 
+    //@RequestParam
+    //http://localhost:8080/get?a=10&b=200
+
     @GetMapping(value = "/get")
     public String toStrin1(@RequestParam int a, @RequestParam int b) {
 
         return "Hello World !!" + a + " -- " + b;
     }
+
+//    @GetMapping(value = "/get")
+//    public String toStrin11(@RequestAttribute int b) {
+//
+//        return "Hello World !!" + a + " -- " + b;
+//    }
+
 }
