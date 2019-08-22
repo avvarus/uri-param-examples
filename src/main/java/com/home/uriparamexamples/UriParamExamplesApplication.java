e4afb141c8f54a3a993c59d6a41525b598cbb4c9
Avvarus <avvaru79.java@mail.com>
2019-08-21 21:06:59 -0400

    First Commit

--- src/main/java/com/home/uriparamexamples/UriParamExamplesApplication.java
+++ src/main/java/com/home/uriparamexamples/UriParamExamplesApplication.java
@@ -1,14 +1,21 @@
 package com.home.uriparamexamples;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class UriParamExamplesApplication {
+public class UriParamExamplesApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(UriParamExamplesApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("UriParamExamplesApplication.run");
     }
 }
