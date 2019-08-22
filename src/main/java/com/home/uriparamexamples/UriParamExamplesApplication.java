bffe1e60538e11231f09d9508e9f9b040d81bc48
Avvarus <avvaru79.java@mail.com>
2019-08-21 21:01:49 -0400

    First Commit

--- src/main/java/com/home/uriparamexamples/UriParamExamplesApplication.java
+++ src/main/java/com/home/uriparamexamples/UriParamExamplesApplication.java
@@ -1,9 +1,9 @@
 package com.home.uriparamexamples;
 
-import lombok.Builder;
+import com.home.uriparamexamples.model.Country;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +15,7 @@ public class UriParamExamplesApplication {
         SpringApplication.run(UriParamExamplesApplication.class, args);
     }
 
-
+    // By default it was added to any mapping @ResponseBody
     //@PathVariable
 
     @GetMapping(value = "/get/{a}/{b}")
@@ -35,7 +35,7 @@ public class UriParamExamplesApplication {
     }
 
 
-    ////http://localhost:8080/get?a=10&b=200
+    ////http://localhost:8080/save?a=10&b=200
 
     @GetMapping(value = "/save")
     public ResponseEntity<String> toStrin11(@RequestParam int a, @RequestParam int b) {
@@ -44,5 +44,35 @@ public class UriParamExamplesApplication {
 
         return ResponseEntity.status(200).body("tesion padadku");
     }
+
+    //http://zetcode.com/springboot/responseentity/
+
+    @GetMapping(value = "/resEntity")
+    public ResponseEntity<Country> getCuntry() {
+
+        Country c = new Country();
+        c.setName("France");
+        c.setPopulation(66984000);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Responded", "MyController");
+        headers.add("First Check", "My World");
+
+        return ResponseEntity.accepted().headers(headers).body(c);
+    }
+
+
+    @GetMapping(value = "/resBody")
+    @ResponseBody
+    public Country resBody() {
+
+        return new Country("India", 879799);
+    }
+
+
+    /**
+     *  Uusally no difference between adding @ResponseBody
+     */
 
 }
